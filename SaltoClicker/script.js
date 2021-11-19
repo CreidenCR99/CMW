551ca26d85f034e1c3fdeefbc525466f7aa74651
CreidenCR99 <adriancuervorodriguez@gmail.com>
2021-11-19 21:26:49 +0100

    initial commit

---
+++ SaltoClicker/script.js
@@ -0,0 +1,21 @@
+var Saltos = 0;
+
+function ClickCabeza() {
+    Saltos ++;
+}
+
+function render60() {
+    document.getElementById("render").innerHTML = Saltos;
+}
+
+function render1() {
+    document.getElementById("title").innerHTML = `Salto Simulator - ${Saltos} Saltos`;
+}
+
+setInterval(function() {
+    render1();
+}, 1000)
+
+setInterval(function() {
+	render60();
+}, 1000 / 60);
