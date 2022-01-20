2ff9d5c784c6948756d4b38f0efb8216d4644ce5
CreidenCR99 <adriancuervorodriguez@gmail.com>
2022-01-21 00:54:46 +0100

    initial commit

--- SaltoSimulator/PC/js/script.js
+++ SaltoSimulator/PC/js/script.js
@@ -119,6 +119,17 @@ function Derecha(Seleccion) {
 	}
 }
 
+function VersionLong() {
+	document.getElementById("VersionShort").style = "display: none;";
+	document.getElementById("VersionLong").style = "display: block;";
+};
+
+function VersionShort() {
+	document.getElementById("VersionLong").style = "display: none;";
+	document.getElementById("VersionShort").style = "display: block;";
+};
+
+
 function render60() {
 	if (Math.floor(Saltos) == 0) {
 		document.getElementById("Saltos").innerHTML = `0 Saltos`;
