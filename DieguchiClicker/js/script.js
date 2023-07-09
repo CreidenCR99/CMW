c8c94a8cc24a23bac192674f6742a2a0e62d3084
CreidenCR99 <adriancuervorodriguez@gmail.com>
2023-07-09 15:35:04 +0200

    v2

--- DieguchiClicker/js/script.js
+++ DieguchiClicker/js/script.js
@@ -2,22 +2,23 @@ var u = 0; // Unidad (m, km)
 var ups = 0; // Unidad por tiempo (m/s, km/h)
 function f_clic() {
     u++;
+	f_ups();
 };
 
 function f_ups() {
 	if (u <= 999) {
 		document.getElementById("id_u").innerHTML =
-		`${u} m`;
+		`${u.toFixed(1)} m`;
 	} else if (u >= 1000) {
 		document.getElementById("id_u").innerHTML =
-		`${u/1000} km`
+		`${(u/1000).toFixed(1)} km`
 	}
 	if (ups <= 999) {
 		document.getElementById("id_ups").innerHTML =
-		`${ups} m/s`;
-	} else if (u >= 1000) {
+		`${ups.toFixed(1)} m/s`;
+	} else if (ups >= 1000) {
 		document.getElementById("id_ups").innerHTML =
-		`${ups/1000} km/s`
+		`${(ups/1000).toFixed(1)} km/s`
 	}
 }
 
@@ -27,4 +28,4 @@ function sleep(ms) {
 
 setInterval(function() {
 	f_ups();
-}, 1000 / 60);
+}, 1000*10);
