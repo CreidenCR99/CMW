dc2ab6942eb89f4ceb5f5761b7a726f837b200b5
CreidenCR99 <adriancuervorodriguez@gmail.com>
2023-07-13 01:49:58 +0200

    v2.3

---
+++ DieguchiClicker/js/cookies.js
@@ -0,0 +1,45 @@
+f_loadCookies();
+
+var AutoSaveTime = 15;
+
+async function af_saveCookies() {
+	var cookieValue = "u:" + u + ",ups:" + ups;
+	document.cookie = "savedData=" + cookieValue;
+	console.log("Cookies saved")
+}
+
+function f_loadCookies() {
+	if(document.cookie || document.cookie=='savedData=u:0,ups:0'){} 
+	else{
+		var cookieValue = document.cookie
+			.split('; ')
+			.find(row => row.startsWith('savedData='))
+			.split('=')[1];
+
+		if (cookieValue) {
+			var valuePairs = cookieValue.split(',');
+			var data = {};
+
+			valuePairs.forEach(function(pair) {
+				var parts = pair.split(':');
+				var name = parts[0];
+				var value = parts[1];
+				data[name] = value;
+			});
+
+			u = parseFloat(data.u);
+			ups = parseFloat(data.ups);
+		}
+		console.log("Cookies loaded")
+	}
+}
+
+
+function Restart() {
+	document.cookie = "savedData=0; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/DieguchiClicker;";
+	window.location.reload();
+};
+
+setInterval(function() {
+	af_saveCookies();
+}, 1000 * AutoSaveTime);
