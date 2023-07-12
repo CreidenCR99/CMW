9cc1e7b564cf46b37afc9fb4d53d3c595812a3e0
CreidenCR99 <adriancuervorodriguez@gmail.com>
2023-07-13 01:49:58 +0200

    v2.3

--- DieguchiClicker/js/script.js
+++ DieguchiClicker/js/script.js
@@ -1,24 +1,31 @@
 var u = 0; // Unidad (m, km)
 var ups = 0; // Unidad por tiempo (m/s, km/h)
+
 function f_clic() {
-    u += 0.1;
+	u += 0.1;
 	f_ups();
 }
 
 function f_ups() {
 	if (u <= 999) {
 		document.getElementById("id_u").innerHTML =
-		`${u.toFixed(1)} m`;
+			`${u.toFixedDown(1)} m`;
 	} else if (u >= 1000) {
 		document.getElementById("id_u").innerHTML =
-		`${(u/1000).toFixed(1)} km`
+			`${(u/1000).toFixedDown(1)} km`
+	} else if ((u / 1000) >= 10) {
+		document.getElementById("id_u").innerHTML =
+			`${(u/1000).toFixedDown(0)} km`
 	}
 	if (ups <= 999) {
 		document.getElementById("id_ups").innerHTML =
-		`${ups.toFixed(1)} m/s`;
+			`${ups.toFixedDown(1)} m/s`;
 	} else if (ups >= 1000) {
 		document.getElementById("id_ups").innerHTML =
-		`${(ups/1000).toFixed(1)} km/s`
+			`${(ups/1000).toFixedDown(1)} km/s`
+	} else if ((ups / 1000) >= 10) {
+		document.getElementById("id_ups").innerHTML =
+			`${(ups/1000).toFixedDown(0)} km/s`
 	}
 }
 
@@ -37,6 +44,16 @@ function f_show(window) {
 		$(".other").show();
 	}
 }
+
+function f_onload() {
+	console.log("Page loaded")
+}
+
+Number.prototype.toFixedDown = function(digits) {
+	var re = new RegExp("(\\d+\\.\\d{" + digits + "})(\\d)"),
+		m = this.toString().match(re);
+	return m ? parseFloat(m[1]) : this.valueOf();
+};
 
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
