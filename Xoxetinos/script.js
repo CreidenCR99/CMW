2c3509f40c58e91df037023676938ea2ee4b359e
CreidenCR99 <adriancuervorodriguez@gmail.com>
2024-06-13 22:52:12 +0200

    update

--- Xoxetinos/script.js
+++ Xoxetinos/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
         {name: "Gabino", date: "Mar 4, 2025", year: "(08)"},
         {name: "Nerea", date: "Mar 18, 2025", year: "(08)"},
         {name: "Lucas", date: "May 19, 2025", year: "(09)"},
-        {name: "Iyan", date: "Jun 18, 2025", year: "(06)"},
+        {name: "Iyan", date: "Jun 18, 2024", year: "(06)"},
         {name: "Val", date: "Jul 14, 2024", year: "(06)"},
         {name: "Deivid", date: "Jul 20, 2024", year: "(05)"},
         {name: "Guille", date: "Jul 21, 2024", year: "(07)"},
@@ -86,18 +86,56 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     // ---------- Frases ---------- \\
 
-    var Frases = [
-        {texto: '"Si mueres, pierdes la vida."', autor: "Shadic", fecha: "2021"},
-        {texto: '"Prendele fuego."', autor: "Io", fecha: "2021"},
-        {texto: '"No quiero acabar morido... De momento"', autor: "Martino", fecha: "2021"},
-        {texto: '"¿Eso por donde se mete?"', autor: "Sergio", fecha: "2022"},
-        {texto: '"En las noches de calor, me meto en el congelador."', autor: "Io", fecha: "2022"},
-        {texto: '"Que descanséis cuando os durmáis."', autor: "Pau", fecha: "2022"},
-        {texto: '"Me la metes en el ascensor."', autor: "Olaya", fecha: "2023"},
-        {texto: '"No sabes como me pones 😜🔥"', autor: "Casque", fecha: "2023"},
-        {texto: '"Glup 🫧"', autor: "Nerea y yo", fecha: "2023"},
-        {texto: '"Si los culos son culos y las tetas son tetas trae tu boca y abre mi bragueta"', autor: "Gabino", fecha: "2023"}
+    var frases = [
+        {id: "A", texto: "Si mueres, pierdes la vida.", autor: "Shadic", fecha: "2021"},
+        {id: "B",texto: "Prendele fuego.", autor: "Io", fecha: "2021"},
+        {id: "C",texto: "No quiero acabar morido... De momento", autor: "Martino", fecha: "2021"},
+        {id: "D",texto: "¿Eso por donde se mete?", autor: "Sergio", fecha: "2022"},
+        {id: "E",texto: "En las noches de calor, me meto en el congelador.", autor: "Io", fecha: "2022"},
+        {id: "F",texto: "Que descanséis cuando os durmáis.", autor: "Pau", fecha: "2022"},
+        {id: "G",texto: "Boicot al euro, que vuelva la peseta.", autor: "Adro", fecha: "2023"},
+        {id: "H",texto: "Me la metes en el ascensor.", autor: "Olaya", fecha: "2023"},
+        {id: "I",texto: "No sabes como me pones 😜🔥", autor: "Casque", fecha: "2023"},
+        {id: "J",texto: "Glup 🫧", autor: "Nerea y yo", fecha: "2023"},
+        {id: "K",texto: "Si los culos son culos y las tetas son tetas trae tu boca y abre mi bragueta", autor: "Gabino", fecha: "2023"}
     ]
+    
+    var contenedorFrases = document.getElementById('contenedor-frases');
+
+    frases.forEach(function(frase) {
+        var div = document.createElement('div');
+        div.className = 'box';
+        div.id = frase.id;
+
+        var h2Texto = document.createElement('h2');
+        h2Texto.id = 'Texto';
+        div.appendChild(h2Texto);
+
+        var h2AutoryFecha = document.createElement('h2');
+        h2AutoryFecha.id = 'AutoryFecha';
+        div.appendChild(h2AutoryFecha);
+
+
+        contenedorFrases.appendChild(div);
+
+        var container = document.querySelector(`#${frase.id}`);
+
+        container.querySelector('#Texto').innerHTML = `"${frase.texto}"`;
+        container.querySelector('#AutoryFecha').innerHTML = frase.autor + " - " + frase.fecha;
+    });
 });
 
-
+import { readFile, utils } from 'xlsx';
+var wb = readFile('tu_archivo.xlsx', { cellDates: true });
+var ws = wb.Sheets['Hoja1']; // Cambia 'Sheet1' al nombre de tu hoja
+const headerRow = utils.sheet_to_json(ws, { header: 1 })[0];
+const parametros = headerRow.slice(1); // Ignora la primera celda (que contiene el título)
+const data = utils.sheet_to_json(ws, { header: 1, range: 1 }); // Ignora la primera fila (encabezados)
+const arrayDeDatos = data.map((fila) => {
+    const obj = {};
+    parametros.forEach((parametro, indice) => {
+        obj[parametro] = fila[indice + 1]; // Ignora la primera celda (que contiene el nombre de la fila)
+    });
+    return obj;
+});
+console.log(arrayDeDatos)
